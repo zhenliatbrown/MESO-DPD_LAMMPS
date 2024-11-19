a4d821197e663d94a0b2b1bf4833b05ebd82e70f
Evan Weinberg <eweinberg@nvidia.com>
2024-11-19 08:56:36 -0800

    Re-enable SNAP CPU codepaths to simplify verifying correctness

--- src/KOKKOS/pair_snap_kokkos_impl.h
+++ src/KOKKOS/pair_snap_kokkos_impl.h
@@ -85,14 +85,14 @@ PairSNAPKokkos<DeviceType, real_type, vector_length>::~PairSNAPKokkos()
 template<class DeviceType, typename real_type, int vector_length>
 void PairSNAPKokkos<DeviceType, real_type, vector_length>::init_style()
 {
-  if (host_flag) {
-    if (lmp->kokkos->nthreads > 1)
-      error->all(FLERR,"Pair style snap/kk can currently only run on a single "
-                         "CPU thread");
+  //if (host_flag) {
+  //  if (lmp->kokkos->nthreads > 1)
+  //    error->all(FLERR,"Pair style snap/kk can currently only run on a single "
+  //                       "CPU thread");
 
-    PairSNAP::init_style();
-    return;
-  }
+  //  PairSNAP::init_style();
+  //  return;
+  //}
 
   if (force->newton_pair == 0)
     error->all(FLERR,"Pair style SNAP requires newton pair on");
@@ -134,12 +134,12 @@ struct FindMaxNumNeighs {
 template<class DeviceType, typename real_type, int vector_length>
 void PairSNAPKokkos<DeviceType, real_type, vector_length>::compute(int eflag_in, int vflag_in)
 {
-  if (host_flag) {
-    atomKK->sync(Host,X_MASK|F_MASK|TYPE_MASK);
-    PairSNAP::compute(eflag_in,vflag_in);
-    atomKK->modified(Host,F_MASK);
-    return;
-  }
+  //if (host_flag) {
+  //  atomKK->sync(Host,X_MASK|F_MASK|TYPE_MASK);
+  //  PairSNAP::compute(eflag_in,vflag_in);
+  //  atomKK->modified(Host,F_MASK);
+  //  return;
+  //}
 
   eflag = eflag_in;
   vflag = vflag_in;
