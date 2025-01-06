f07c28a7a129a346453baa564a9d01e82f0f5306
OfirBlumer <62602693+OfirBlumer@users.noreply.github.com>
2025-01-06 17:57:27 +0200

    Update bosonic_exchange.cpp

--- src/REPLICA/bosonic_exchange.cpp
+++ src/REPLICA/bosonic_exchange.cpp
@@ -1,3 +1,27 @@
+/* ----------------------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+/* ----------------------------------------------------------------------
+   Package      BosonicExchange
+
+   Purpose      Handle Bosonic Exchange in Path Integral Molecular Dynamics
+   Copyright    Hirshberg lab @ Tel Aviv University
+   Authors      Yotam M. Y. Feldman, Ofir Blumer
+
+   Updated      Jan-06-2025
+   Version      1.0
+------------------------------------------------------------------------- */
+
 #include "bosonic_exchange.h"
 
 #include "domain.h"
