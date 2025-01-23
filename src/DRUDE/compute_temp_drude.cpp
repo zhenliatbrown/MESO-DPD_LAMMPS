57cc7b6817bd67885920610ebcbe1e35fa3b0883
Axel Kohlmeyer <akohlmey@gmail.com>
2025-01-23 06:00:41 -0500

    programming style and error message updates

--- src/DRUDE/compute_temp_drude.cpp
+++ src/DRUDE/compute_temp_drude.cpp
@@ -30,7 +30,7 @@ using namespace LAMMPS_NS;
 
 ComputeTempDrude::ComputeTempDrude(LAMMPS *lmp, int narg, char **arg) : Compute(lmp, narg, arg)
 {
-  if (narg != 3) error->all(FLERR, "Illegal compute temp command");
+  if (narg != 3) error->all(FLERR, "Incorrect number of arguments for compute temp/drude");
 
   vector_flag = 1;
   scalar_flag = 1;
@@ -63,11 +63,13 @@ void ComputeTempDrude::init()
 {
   // Fix drude already checks that there is only one fix drude instance
   auto &fixes = modify->get_fix_by_style("^drude$");
-  if (fixes.size() == 0) error->all(FLERR, "compute temp/drude requires fix drude");
+  if (fixes.size() == 0)
+    error->all(FLERR, Error::NOLASTLINE, "compute temp/drude requires fix drude");
   fix_drude = dynamic_cast<FixDrude *>(fixes[0]);
 
   if (!comm->ghost_velocity)
-    error->all(FLERR, "compute temp/drude requires ghost velocities. Use comm_modify vel yes");
+    error->all(FLERR, Error::NOLASTLINE,
+               "compute temp/drude requires ghost velocities. Use comm_modify vel yes");
 }
 
 /* ---------------------------------------------------------------------- */
@@ -170,7 +172,7 @@ void ComputeTempDrude::compute_vector()
             error->one(FLERR, "Drude atom for core atom ID {} is not defined", atom->tag[i]);
           } else {
             error->one(FLERR, "Drude atom ID {} for core atom ID {} is out of range", drudeid[i],
-                     atom->tag[i]);
+                       atom->tag[i]);
           }
         }
         if (rmass) {
