89abf65751ef9d72a417e369fee77113ea887949
Richard Berger <richard.berger@outlook.com>
2025-02-15 00:36:06 -0700

    python: remove unused import in core.py

--- python/lammps/core.py
+++ python/lammps/core.py
@@ -17,7 +17,6 @@
 from __future__ import print_function
 
 import os
-import sys
 from ctypes import CDLL, POINTER, RTLD_GLOBAL, CFUNCTYPE, py_object, byref, cast, sizeof, \
   create_string_buffer, c_int, c_int32, c_int64, c_double, c_void_p, c_char_p, c_char,    \
   pythonapi
